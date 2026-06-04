3c9f26f379522cb594698feaff78551ecb0d5266
Shujah Uz Zaman <shujauzzaman20@gmail.com>
2026-06-04 01:21:36 -0700

    home link to auth

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Home () {
   return (
     <div>
-      Home
+      <Link
+          to="/authenticate"
+          className=" bg-brand hover:bg-brand-accent text-white text-sm font-medium px-4 py-2 rounded-md transition-colors whitespace-nowrap"
+      >
+          Authenticate
+      </Link>
     </div>
   )
 }
